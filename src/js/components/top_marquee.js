0c12ab612c12c6ba03d41e98695de5d29a42dd90
9stars <9@u9.ru>
2024-10-08 19:55:28 +0300

    Сохранения состояния скрытия бегущей строки

--- src/js/components/top_marquee.js
+++ src/js/components/top_marquee.js
@@ -1,11 +1,19 @@
 import * as $ from "jquery";
+import store from "store";
 
 $(function() {
     if($('.marquee2').length == 1){
+        let st = store.get('top_marquee');
+        if(st == 'hidden'){
+            $('.marquee2__close').hide();
+            $('.marquee2').hide();
+            $('header').removeClass('with_marquee');
+        }
         $('.marquee2__close').on('click',function(){
             $(this).hide();
             $('.marquee2').hide();
             $('header').removeClass('with_marquee');
+            store.set('top_marquee', 'hidden')
         })
     }
 });
